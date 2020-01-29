2751e173642427e83ccdbe261dd9b3a713221568
happyso <cheza1220@gmail.com>
2020-01-29 22:09:57 +0900

    initial get function

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('hi!');
+const express = require('express')
+const app = express();
+const PORT = 4000;
+
+const handleListening = () => { 
+    console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+const handleHome = (req, res) => {
+    res.send("Hi from Home!");
+}
+
+const handleProfile = (req, res) =>{
+    res.send("you are on my profile");
+}
+app.get('/', handleHome);
+app.get ("/profile",handleProfile)
+ 
+app.listen(PORT, handleListening);
