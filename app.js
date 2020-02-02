d735bde8547fa00cde21a89a0729169401188ed5
happyso <cheza1220@gmail.com>
2020-02-02 18:13:29 +0900

    set controller

--- app.js
+++ app.js
@@ -3,26 +3,24 @@ import morgan from "morgan"; //middleware
 import helmet from "helmet";//middleware 보안
 import cookieParser from "cookie-parser";//middleware
 import bodyParser from "body-parser";//middleware
-import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 const app = express();
 
-const handleHome = (req, res) => {res.send("Hi from Home!");}
-
-const handleProfile = (req, res) =>{res.send("you are on my profile");}
-
-
 app.use(cookieParser());//유저로부터받은 cookie이해를 위해
-app.use(bodyParser.json({extended : true})); //서버가 유저로부터 받은 데이터 이해를 위해
+app.use(bodyParser.json()); //서버가 유저로부터 받은 데이터 이해를 위해
 app.use(bodyParser.urlencoded({extended : true})); //서버가 유저로부터 받은 데이터 이해를 위해
 app.use(helmet());
-app.use(morgan("dev"));
+app.use(morgan("dev")); //logging 
 
-
-app.get('/',  handleHome);
-
-app.get ("/profile", handleProfile);
-app.use ("/user", userRouter);
+app.use (routes.home, globalRouter);
+app.use (routes.users, userRouter);
+app.use (routes.videos, videoRouter);
 
 
 export default app;
+
+//mvc module data view look control funstion that looks for data
