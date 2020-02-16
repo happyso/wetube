e24f5869893935e46447462b448a8c7c26888d43
happyso <cheza1220@gmail.com>
2020-02-16 11:26:54 +0900

    mongodb setting

--- db.js
+++ db.js
@@ -1,50 +1,21 @@
-export const videos = [
+import mongooes from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+mongooes.connect(
+    process.env.MONGO_URL,
     {
-        id: 324393,
-        title: "Video awesome",
-        description: "This is something I love",
-        views: 24,
-        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "Nicolas",
-            email: "nico@las.com"
-        }
-    },
-    {
-        id: 1212121,
-        title: "Video super",
-        description: "This is something I love",
-        views: 24,
-        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "Nicolas",
-            email: "nico@las.com"
-        }
-    },
-    {
-        id: 55555,
-        title: "Video nice",
-        description: "This is something I love",
-        views: 24,
-        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "Nicolas",
-            email: "nico@las.com"
-        }
-    },
-    {
-        id: 11111,
-        title: "Video perfect",
-        description: "This is something I love",
-        views: 24,
-        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "Nicolas",
-            email: "nico@las.com"
-        }
+    useNewUrlParser: true,
+    useFindAndModify: false
     }
-];
+);
+
+const db = mongooes.connection;
+
+const handleOpen = () => console.log("connect to DB");
+const handleError = error => console.log(`X Error on DB connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error",handleError);
