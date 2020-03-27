d7674d1dee83b99f9c24b0299974c59bfb79d95d
happyso <cheza1220@gmail.com>
2020-03-27 11:17:39 +0900

    commit 11.4

--- src/app.js
+++ src/app.js
@@ -1,29 +1,30 @@
-import express from 'express';
-import morgan from 'morgan'; // middleware
-import helmet from 'helmet'; // middleware 보안
-import cookieParser from 'cookie-parser'; // middleware
-import bodyParser from 'body-parser'; // middleware
-import passport from 'passport';
-import mongoose from 'mongoose';
-import session from 'express-session';
-import MongoStore from 'connect-mongo';
-import {
-    localMiddleware
-} from './middlewares';
-import routes from './routes';
-import userRouter from './routers/userRouter';
-import videoRouter from './routers/videoRouter';
-import globalRouter from './routers/globalRouter';
-import apiRouter from './routers/apiRouter';
-import './passport';
+  
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import passport from "passport";
+import mongoose from "mongoose";
+import session from "express-session";
+import path from "path";
+import MongoStore from "connect-mongo";
+import { localMiddleware } from "./middlewares";
+import routes from "./routes";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import apiRouter from "./routers/apiRouter";
+
+import "./passport";
 
 const app = express();
 const CokieStore = MongoStore( session );
 
 app.use( helmet() );
 app.set( 'view engine', 'pug' );
-app.use( '/uploads', express.static( 'uploads' ) );
-app.use( '/static', express.static( 'static' ) );
+app.set("views", path.join(__dirname, "views"));
+app.use("/static", express.static(path.join(__dirname, "static")));
 app.use( cookieParser() ); // 유저로부터받은 cookie이해를 위해
 app.use( bodyParser.json() ); // 서버가 유저로부터 받은 데이터 이해를 위해
 app.use( bodyParser.urlencoded( {
